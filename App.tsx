48bbf32ec54f0bfbc9fe823675f911c02a286e1d
Anthony G <tonixoskv7@gmail.com>
2026-03-16 23:33:02 -0500

    fix - correccion de safeAreaView para todos los dispositivos

--- App.tsx
+++ App.tsx
@@ -2,13 +2,20 @@ import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ThemeProvider, useTheme } from './src/theme/ThemeContext';
+import { useNotifications } from './src/hooks/useNotifications';
 import AppNavigator from './src/navigation/AppNavigator';
 
 function ThemedApp() {
   const { isDark } = useTheme();
+  const { checkAndScheduleAlerts } = useNotifications();
+
+  React.useEffect(() => {
+    checkAndScheduleAlerts();
+  }, [checkAndScheduleAlerts]);
+
   return (
     <>
-      <StatusBar style={isDark ? 'light' : 'dark'} />
+      <StatusBar style={isDark ? 'light' : 'dark'} backgroundColor="transparent" translucent />
       <AppNavigator />
     </>
   );
